741d70dc94b05d7f4a7838c996c61eb56c8a70bc
Timani G <timanig@gmail.com>
2019-11-07 13:56:18 -0500

    /Extension Methods
    * Creating Extenstion Methods *

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = "Hello World";
+
+            Console.WriteLine($"If {x} is two words print " +
+                $"true else print false: {x.IsTwoWords()} ");
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/StringExtenstions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ExtensionMethods
+{
+    public static class StringExtenstions
+    {
+        
+            public static bool IsTwoWords(this string s)
+            {
+            if (s.Contains(" "))
+                return true;
+               return false;
+            }
+        
+    }
+}
